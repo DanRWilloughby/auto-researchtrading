17e275e7e69a32a259e960f81584870c08e118a8
Daniel Willoughby <dan@newfamiliar.co>
2026-03-22 13:43:54 -0400

    exp15: shorter momentum 10/20/60->8/16/48

--- strategies/30m-btc-eth-sol/strategy.py
+++ strategies/30m-btc-eth-sol/strategy.py
@@ -10,9 +10,9 @@ ACTIVE_SYMBOLS = ["BTC", "ETH", "SOL"]
 SYMBOL_WEIGHTS = {"BTC": 0.33, "ETH": 0.33, "SOL": 0.33}
 
 # Lookback windows (roughly 1.5-2x the 1h values to cover similar time periods)
-SHORT_WINDOW = 10     # 5 hours
-MED_WINDOW = 20       # 10 hours
-LONG_WINDOW = 60      # 30 hours
+SHORT_WINDOW = 8      # 4 hours
+MED_WINDOW = 16       # 8 hours
+LONG_WINDOW = 48      # 24 hours
 EMA_FAST = 10
 EMA_SLOW = 42
 RSI_PERIOD = 10
